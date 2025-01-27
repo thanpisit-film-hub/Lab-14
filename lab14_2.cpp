8f8d2cbe88876e9b1607b2a669b37460e3cb2056
thanpisit-film-hub <tanpisit.2548@gmail.com>
2025-01-27 10:28:21 +0700

    Lab14

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,5 +19,34 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool A[][N]){
+    for(int i = 0;i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << A[i][j] << " ";
+        }
+        cout << " \n";
+    }
+}
+void inputMatrix(double A[][N]){
+    for(int i = 0; i < N; i++){
+        cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < N; j++) {
+            cin >> A[i][j];
+        }
+    }
+}
+void findLocalMax(const double A[][N], bool B[][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            B[i][j] = false;
+        }
+    }
 
-//Do not modify source code above this line 
+    for (int i = 1; i < N-1; i++) {
+        for (int j = 1; j < N-1; j++) {
+            if (A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j] && A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1]) {
+                B[i][j] = true;
+            }
+        }
+    }
+}
